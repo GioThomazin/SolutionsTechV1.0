d6d13ab3bf628e62721789fc35cd4dd579a3fedf
@GioThomazin <githomazin@gmail.com>
2025-03-26 22:33:51 -0300

    fluentValidation

---
+++ src/SolutionsTech.MVC/Validations/UserValidation/UserValidation.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using SolutionsTech.Business.Entity;
+
+namespace SolutionsTech.MVC.Validations.UserValidation
+{
+	public class UserValidation : AbstractValidator<User>
+	{
+		public UserValidation()
+		{
+			RuleFor(x => x.Name)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Campo Obrigatório");
+
+			RuleFor(x => x.Address)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Campo Obrigatório");
+
+			RuleFor(x => x.DtCreate)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Campo Obrigatório");
+
+			RuleFor(x => x.DtBorn)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Campo Obrigatório");
+
+
+			RuleFor(x => x.Cel)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Campo Obrigatório");
+		}
+	}
+}
