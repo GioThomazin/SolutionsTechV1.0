e09e5b07ecbdda4b05a8770140a74013d699fb19
@GioThomazin <githomazin@gmail.com>
2025-03-31 21:33:05 -0300

    Select 2 TypeProcedure

--- src/SolutionsTech.MVC/Dtos/SchedulingDto.cs
+++ src/SolutionsTech.MVC/Dtos/SchedulingDto.cs
@@ -13,6 +13,7 @@ namespace SolutionsTech.MVC.Dto
 		public string Observation { get; set; }
 		public long IdUser { get; set; }
 		public long IdFormPayment { get; set; }
+		public long IdTypeProcedure { get; set; }
 		public UserDto? User { get; set; }
 		public IEnumerable<UserDto?> Users { get; set; } = new List<UserDto?>();
 
@@ -20,5 +21,8 @@ namespace SolutionsTech.MVC.Dto
 
 		public IEnumerable<FormPaymentDto?> FormPayments { get; set; } = new List<FormPaymentDto?>();
 
+		public TypeProcedureDto? TypeProcedure { get; set; }
+
+		public IEnumerable<TypeProcedureDto?> TypeProcesdures { get; set; } = new List<TypeProcedureDto?>();
 	}
 }
