6ed8715a72a84c9edc6e54a493ae85157cbe47d8
@GioThomazin <githomazin@gmail.com>
2025-04-10 11:28:53 -0300

    ajustes no banco

--- src/SolutionsTech.MVC/Dtos/SchedulingDto.cs
+++ src/SolutionsTech.MVC/Dtos/SchedulingDto.cs
@@ -3,10 +3,9 @@
 	public class SchedulingDto
 	{
 		public long IdScheduling { get; set; }
-		public string Name { get; set; }
 		public DateTime DtCreate { get; set; } = DateTime.Now;
 		public decimal TotalValue { get; set; }
-		public string Observation { get; set; }
+		public string? Observation { get; set; }
 		public long IdUser { get; set; }
 		public long IdFormPayment { get; set; }
 		public long IdTypeProcedure { get; set; }
