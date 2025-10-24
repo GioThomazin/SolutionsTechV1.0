714c029aa4458d1e9914958e513f8546add05577
@GioThomazin <githomazin@gmail.com>
2025-10-24 11:15:25 -0300

    Sincronizando atualizações do repositório SolutionsTech

--- src/SolutionsTech.MVC/Program.cs
+++ src/SolutionsTech.MVC/Program.cs
@@ -11,15 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(ModelMapper));
 builder.Services.AddMvc();
-
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 				.AddRoles<IdentityRole>()
 				.AddEntityFrameworkStores<ApplicationDbContext>();
-
 
 builder.Services.AddControllersWithViews(options =>
 {
@@ -31,6 +30,12 @@ builder.Services.RegisterServices();
 var app = builder.Build();
 
 var cultureInfo = new CultureInfo("pt-BR");
+cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+cultureInfo.NumberFormat.CurrencyDecimalSeparator = ".";
+
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
 app.UseRequestLocalization(new RequestLocalizationOptions
 {
 	DefaultRequestCulture = new RequestCulture(cultureInfo),
@@ -46,10 +51,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthentication(); // Adicionando o middleware de autenticação
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -58,7 +61,6 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-// Em Program.cs (ASP.NET Core 6+)
 app.MapControllerRoute(
 	name: "login",
 	pattern: "Login",
